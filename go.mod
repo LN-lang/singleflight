65ba456d5c673608979dd34d154d35897896bef5
liuna <366236187@qq.com>
2026-01-05 18:41:05 +0800

    singleflight

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/LN-lang/singleflightt
+
+go 1.25.5
+
+require (
+	github.com/go-rod/rod v0.116.2
+	github.com/redis/go-redis/v9 v9.17.2
+	golang.org/x/sync v0.19.0
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/ysmood/fetchup v0.2.3 // indirect
+	github.com/ysmood/goob v0.4.0 // indirect
+	github.com/ysmood/got v0.40.0 // indirect
+	github.com/ysmood/gson v0.7.3 // indirect
+	github.com/ysmood/leakless v0.9.0 // indirect
+)
---
+++ singleflight/singleflight.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-rod/rod"
+	"github.com/redis/go-redis/v9"
+	"golang.org/x/sync/singleflight"
+)
+
+var (
+	rdb     = redis.NewClient(&redis.Options{Addr: " 101.126.43.173:6379 "})
+	sfGroup singleflight.Group
+	ctx     = context.Background()
+)
+
+// 核心：热点数据获取（缓存+请求合并）
+func getHotData(key string) string {
+	// 1. 查缓存
+	if v, _ := rdb.Get(ctx, key).Result(); v != "" {
+		return v
+	}
+	// 2. 合并请求查数据源+写缓存
+	val, _, _ := sfGroup.Do(key, func() (interface{}, error) {
+		// ========== 替换这里：你的爬虫逻辑 ==========
+		browser := rod.New().MustConnect()
+		defer browser.MustClose()
+		page := browser.MustPage(" https://air.1688.com/kapp/channel-fe/cps-4c-pc/sytm?type=1&offerIds=660390230106,574965204819,949033739317 ")
+		page.MustWaitLoad()
+
+		// 提取商品名称+价格（核心爬取逻辑）
+		name, _ := page.MustElementX("//*[@id=\"ice-container\"]/div/div/div[3]/a[1]/div/div[1]").Text()
+		price, _ := page.MustElementX("//*[@id=\"ice-container\"]/div/div/div[3]/a[1]/div/div[3]").Text()
+		data := fmt.Sprintf("商品：%s | 价格：%s", name, price)
+		// ==========================================
+
+		rdb.Set(ctx, key, data, 5*60) // 缓存5分钟
+		return data, nil
+	})
+	return val.(string)
+}
+
+func main() {
+	fmt.Printf("✅ 热点数据：%s\n", getHotData("goods:660390230106"))
+}
